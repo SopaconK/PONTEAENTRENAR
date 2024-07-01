28425540c2336df48744a8669aa63ccd2586858b
sopaconk <emmanuelbb2000@gmail.com>
2024-07-01 10:09:11 -0600

    Second try subtask2 B D1

--- PES-IOI/Ex1/b.cpp
+++ PES-IOI/Ex1/b.cpp
@@ -9,19 +9,41 @@ vector<int> countScans(vector<int> A,vector<int> X,vector<int> V){
 	int n=A.size();
     int q=X.size();
     vector<int> sv;
-    for(int i=0; i<q; ++i){
-        A[X[i]]=V[i];
-        int ans=0;
-        for(int j=0; j<n; ++j){
-            int aux=0;
-            for(int k=0; k<j; ++k){
-                if(A[k]>A[j]){
-                    aux++;
-                }
+    vector<int> val (n);
+    for(int j=0; j<n; ++j){
+        int aux=0;
+        for(int k=0; k<j; ++k){
+            if(A[k]>A[j]){
+                aux++;
             }
-            ans=max(ans, aux);
+        }
+        val[j]=aux;
+    }
+    for(int i=0; i<q; ++i){
+
+        for(int j=0; j<X[i]; ++j){
+            if(A[j]>A[X[i]]){
+                val[X[i]]--;
+            }
+            if(A[j]>V[i]){
+                val[X[i]]++;
+            }
+        }
+        for(int j=X[i]+1; j<n; ++j){
+            if(A[X[i]]>A[j]){
+                val[j]--;
+            }
+            if(V[i]>A[j]){
+                val[j]++;
+            }
+        }
+        int ans=0;
+        for(int x: val){
+            ans=max(ans, x);
         }
         sv.pb(ans);
+        A[X[i]]=V[i];
+
     }
     return sv;
 }
