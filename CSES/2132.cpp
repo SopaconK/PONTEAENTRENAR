6c90b9aeab8c752dd4b673f4d81c60b37608f0af
sopaconk <emmanuelbb2000@gmail.com>
2024-07-16 18:24:02 -0600

    CSES 2132

---
+++ CSES/2132.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+using lli=long long int;
+#define pb push_back
+#define deb(x) cout<<#x<<": "<<x<<endl;
+//#define endl '\n'
+
+void solve(){
+    lli n;
+    cin>>n;
+    priority_queue<lli> pq;
+    lli ans=0;
+    lli x;
+    cin>>x;
+    pq.push(x);
+    
+    for(lli i=1; i<n; ++i){
+        cin>>x;
+        x-=i;
+        lli val=pq.top();
+        pq.push(x);
+        pq.push(x);
+        pq.pop();
+        if(val>=x){
+            ans+=abs(val-x);
+        }
+       
+    }
+    cout<<ans<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    lli t=1;
+    //cin>>t;
+    while (t--){
+        solve();
+    }
+}
